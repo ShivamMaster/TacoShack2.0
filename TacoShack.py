b532072a506819adcd456400d63a6be4a1667481
Shivam Goel <Shivam0922@gmail.com>
2022-12-03 16:04:14 -0500

    Add files via upload

---
+++ TacoShack.py
@@ -0,0 +1,22 @@
+import pyautogui
+import time
+import random
+
+time.sleep(4)
+ 
+command1 = 0      #control when actual tip, work, and overtime command will run
+command2 = 0      #Controls the loop that runs controls when the tip, work, and overtime command will run
+
+
+while (command2 < 101):
+    while (command1 < 1):   
+        for line in open("TacoShackCommands.txt", "r"):
+            pyautogui.typewrite(line)
+            time.sleep(2)
+            pyautogui.press("enter")
+            pyautogui.press("enter")
+        command1 = command1 + 1
+    
+    command2 = command2 + 1
+    time.sleep(300 + random.randint(0,60))
+    command1 = 0 
